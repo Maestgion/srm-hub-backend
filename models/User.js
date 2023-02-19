6c6ce7181e6f7e555163493838ef7834a1f88897
Abhishek Dubey <7abhishek1410@gmail.com>
2023-02-20 03:09:58 +0530

    login-done

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const userSchema = mongoose.Schema(
     {   
@@ -20,8 +22,42 @@ const userSchema = mongoose.Schema(
             type:String,
             required: true
         },
+        tokens:
+       [
+        {
+            token:{
+                type: String,
+                required: true
+            }
+        }
+       ]
     }, {timestamps: true}
 )
+
+userSchema.pre('save', async function(next){
+    if(this.isModified('password'))
+    {
+        this.password=await bcrypt.hash(this.password, 10)
+        this.cnfPassword = await bcrypt.hash(this.cnfPassword, 10)
+
+    }
+    next()
+})
+
+userSchema.methods.generateToken = async function()
+{
+    try{
+        let generatedToken = jwt.sign({_id: this._id, }, process.env.SECRET_KEY)
+
+        this.tokens = this.tokens.concat({token:generatedToken})
+        await this.save()
+        return generatedToken;
+    }catch(e)
+    {
+    console.log(e);
+
+    }
+}
 
 const User = mongoose.model("registration", userSchema)
 
--- router/auth.js
+++ router/auth.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcryptjs")
 const User = require("../models/User")
 
 router.post("/register", async (req, res)=>{
@@ -45,6 +46,44 @@ router.post("/register", async (req, res)=>{
     }
 })
 
+router.post("/login", async (req, res)=>{
+
+    const {email, password} = req.body
+
+    if(!username || !password)
+    {
+        res.status(422).json({error:"Please fill all the details"});
+    } 
+
+    try{
+
+        const userExists = await User.findOne({email})
+
+        const passwordMatch = bcrypt.compare(password, userExists.password )
+
+        if(!passwordMatch)
+        {
+            res.status(401).json({error:"Wrong credentials!"})
+
+        }
+        else{
+            const token = await userExists.generateToken()
+            console.log(token)
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 2592000000),
+              httpOnly: true
+            })
+
+            const {password, cnfPassword, ...others} = userExists._doc
+            res.status(201).json({message: "login successful", others})
+            console.log(userExists)
+        }
+    }catch(e)
+    {
+        console.log(e)
+    }
+
+})
 
 
 
